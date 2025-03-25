2b9c89bc737e68775cf40bb53aba7d575dd5bbbd
mistersecret312 <sergiigamer7@gmail.com>
2025-03-25 17:57:10 +0200

    Pedestal

--- src/main/java/com/mistersecret312/thaumaturgy/blocks/PedestalBlock.java
+++ src/main/java/com/mistersecret312/thaumaturgy/blocks/PedestalBlock.java
@@ -2,9 +2,14 @@ package com.mistersecret312.thaumaturgy.blocks;
 
 import com.mistersecret312.thaumaturgy.block_entities.PedestalBlockEntity;
 import com.mistersecret312.thaumaturgy.init.BlockEntityInit;
+import com.mistersecret312.thaumaturgy.init.NetworkInit;
+import com.mistersecret312.thaumaturgy.network.packets.UpdatePedestalClientboundPacket;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
@@ -34,29 +39,52 @@ public class PedestalBlock extends Block implements EntityBlock
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand,
                                  BlockHitResult hit)
     {
-        if(level.isClientSide())
-            return InteractionResult.FAIL;
-
         ItemStack stack = player.getItemInHand(hand);
 
-        if(stack != null && level.getBlockEntity(pos) instanceof PedestalBlockEntity pedestal)
+        if(level.getBlockEntity(pos) instanceof PedestalBlockEntity pedestal)
         {
             if(!stack.isEmpty())
-            {
-                pedestal.setStored(stack);
-                player.setItemInHand(hand, ItemStack.EMPTY);
-                return InteractionResult.SUCCESS;
-            }
+                putItem(pedestal, level, player, pos, stack);
             else
-            {
-                player.setItemInHand(hand, pedestal.getStored());
-                pedestal.setStored(ItemStack.EMPTY);
-                return InteractionResult.SUCCESS;
-            }
+                takeItem(pedestal, level, player, pos);
+
+            return InteractionResult.CONSUME;
 
         }
 
-        return InteractionResult.FAIL;
+        return InteractionResult.PASS;
+    }
+
+    public void takeItem(PedestalBlockEntity pedestal, Level level, Player player, BlockPos pos)
+    {
+        ItemStack stack = pedestal.getDisplayItem().copy();
+        player.addItem(stack);
+        pedestal.setDisplayItem(ItemStack.EMPTY);
+
+        level.playSound(null, pos, SoundEvents.ITEM_FRAME_REMOVE_ITEM, SoundSource.BLOCKS, 1, 1);
+    }
+
+    public void putItem(PedestalBlockEntity pedestal, Level level, Player player, BlockPos pos, ItemStack heldItem)
+    {
+        ItemStack stack = heldItem.copy();
+        stack.setCount(1);
+
+        pedestal.setDisplayItem(stack);
+        if(!player.isCreative())
+            heldItem.shrink(1);
+
+        level.playSound(null, pos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 1, 1);
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston)
+    {
+        super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
+        if(pLevel.getBlockEntity(pPos) instanceof PedestalBlockEntity pedestal)
+        {
+            ItemEntity item = new ItemEntity(pLevel, pPos.getX(), pPos.getY(), pPos.getZ(), pedestal.getDisplayItem().copy());
+            pLevel.addFreshEntity(item);
+        }
     }
 
     @Override
