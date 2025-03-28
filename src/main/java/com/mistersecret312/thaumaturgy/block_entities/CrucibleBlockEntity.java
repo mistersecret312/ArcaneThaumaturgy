73ade9d1b7e13c4177c674892edf5ae799f03e34
SGUDestiny <panormov2006@gmail.com>
2025-03-28 23:36:03 +0400

    Crucible beginning

--- src/main/java/com/mistersecret312/thaumaturgy/block_entities/CrucibleBlockEntity.java
+++ src/main/java/com/mistersecret312/thaumaturgy/block_entities/CrucibleBlockEntity.java
@@ -2,16 +2,84 @@ package com.mistersecret312.thaumaturgy.block_entities;
 
 import com.mistersecret312.thaumaturgy.init.BlockEntityInit;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
+import static com.mistersecret312.thaumaturgy.blocks.CrucibleBlock.IS_BOILING;
+
 public class CrucibleBlockEntity extends BlockEntity
 {
+    private int waterLevel;
+    private int itemsCrafted;
 
     public CrucibleBlockEntity(BlockPos pPos, BlockState pBlockState)
     {
         super(BlockEntityInit.CRUCIBLE.get(), pPos, pBlockState);
+        waterLevel = 0; //Cap 4
+        itemsCrafted = 0; //Cap 4, 4 items per 1 water level
     }
 
+    public static void tick(Level level, BlockPos pos, BlockState state, CrucibleBlockEntity crucible)
+    {
+        if (!level.isClientSide)
+        {
+            int waterLevel = crucible.getWaterLevel();
 
+            if (waterLevel > 0 && crucible.getBlockState().getValue(IS_BOILING))
+            {
+                handleCrafting(crucible);
+            }
+        }
+    }
+
+    //Do crafting stuff here
+    private static void handleCrafting(CrucibleBlockEntity crucible)
+    {
+
+    }
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(int newLevel) {
+        waterLevel = newLevel;
+    }
+
+    public int getItemsCrafted() {
+        return itemsCrafted;
+    }
+
+    public void setItemsCrafted(int newItems) {
+        itemsCrafted = newItems;
+    }
+
+    @Override
+    public void load(CompoundTag compound) {
+        super.load(compound);
+        waterLevel = compound.getInt("water_level");
+        itemsCrafted = compound.getInt("items_left");
+    }
+
+    @Override
+    public void saveAdditional(CompoundTag compound) {
+        super.saveAdditional(compound);
+        compound.putInt("water_level", waterLevel);
+        compound.putInt("items_left", itemsCrafted);
+    }
+
+    @Override
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    private void markUpdated() {
+        super.setChanged();
+        if (level != null)
+            level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), Block.UPDATE_CLIENTS);
+    }
 }
--- src/main/java/com/mistersecret312/thaumaturgy/blocks/CrucibleBlock.java
+++ src/main/java/com/mistersecret312/thaumaturgy/blocks/CrucibleBlock.java
@@ -1,48 +1,45 @@
 package com.mistersecret312.thaumaturgy.blocks;
 
 import com.mistersecret312.thaumaturgy.block_entities.CrucibleBlockEntity;
+import com.mistersecret312.thaumaturgy.init.BlockEntityInit;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.FluidTags;
-import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.level.material.LavaFluid;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.client.extensions.common.IClientBlockExtensions;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Consumer;
-
-public class CrucibleBlock extends Block implements EntityBlock
+public class CrucibleBlock extends BaseEntityBlock
 {
-    public static final IntegerProperty FILL_LEVEL = IntegerProperty.create("fill_level", 0, 4);
     public static final BooleanProperty IS_BOILING = BooleanProperty.create("is_boiling");
 
     public CrucibleBlock(Properties pProperties)
     {
         super(pProperties);
-        this.defaultBlockState().setValue(FILL_LEVEL, 0).setValue(IS_BOILING, false);
+        this.defaultBlockState().setValue(IS_BOILING, false);
     }
 
     @Override
@@ -51,48 +48,66 @@ public class CrucibleBlock extends Block implements EntityBlock
         return Shapes.join(Shapes.block(), Shapes.or(box(0.0D, 0.0D, 4.0D, 16.0D, 3.0D, 12.0D), box(4.0D, 0.0D, 0.0D, 12.0D, 3.0D, 16.0D), box(2.0D, 0.0D, 2.0D, 14.0D, 3.0D, 14.0D), box(2.0D, 4.0D, 2.0D, 14.0D, 16.0D, 14.0D)), BooleanOp.ONLY_FIRST);
     }
 
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
+    }
+
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand,
                                  BlockHitResult pHit)
     {
-        ItemStack stack = pPlayer.getItemInHand(pHand);
-        if(stack.is(Items.WATER_BUCKET))
+        if (pLevel.getBlockEntity(pPos) instanceof CrucibleBlockEntity crucible)
         {
-            pLevel.setBlockAndUpdate(pPos, pState.setValue(FILL_LEVEL, 1));
-            pLevel.playSound(pPlayer, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.BUCKET_EMPTY, SoundSource.PLAYERS, 1.0F, 1.0F);
-            if (!pPlayer.isCreative())
+            ItemStack stack = pPlayer.getItemInHand(pHand);
+            int waterLevel = crucible.getWaterLevel();
+            int itemsLeft = crucible.getItemsCrafted();
+
+            if (waterLevel < 1)
             {
-                pPlayer.setItemInHand(pHand, Items.BUCKET.getDefaultInstance());
+                if (stack.is(Items.WATER_BUCKET))
+                {
+                    crucible.setWaterLevel(4);
+                    if (!pPlayer.isCreative())
+                    {
+                        pPlayer.setItemInHand(pHand, Items.BUCKET.getDefaultInstance());
+                    }
+                    pLevel.playSound(pPlayer, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.BUCKET_EMPTY, SoundSource.PLAYERS, 1.0F, 1.0F);
+                    return InteractionResult.SUCCESS;
+                }
+            } else if (waterLevel < 4)
+            {
+                if (stack.is(Items.POTION) && PotionUtils.getPotion(stack) == Potions.WATER)
+                {
+                    crucible.setWaterLevel(waterLevel + 1);
+                    if (!pPlayer.isCreative())
+                    {
+                        pPlayer.setItemInHand(pHand, Items.GLASS_BOTTLE.getDefaultInstance());
+                    }
+                    pLevel.playSound(null, pPos, SoundEvents.BOTTLE_EMPTY, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    return InteractionResult.SUCCESS;
+                }
             }
-            return InteractionResult.sidedSuccess(pLevel.isClientSide());
         }
-        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+        return InteractionResult.PASS;
     }
 
     @Override
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext pContext)
     {
-        return this.defaultBlockState().setValue(FILL_LEVEL, 0).setValue(IS_BOILING, false);
+        return this.defaultBlockState().setValue(IS_BOILING, false);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder)
     {
-        pBuilder.add(FILL_LEVEL);
         pBuilder.add(IS_BOILING);
         super.createBlockStateDefinition(pBuilder);
     }
 
     @Override
-    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state)
-    {
-        return new CrucibleBlockEntity(pos, state);
-    }
-
-    @Override
     public BlockState updateShape(BlockState pState, Direction pDirection, BlockState pNeighborState, LevelAccessor pLevel, BlockPos pPos, BlockPos pNeighborPos)
     {
-        return pState.setValue(IS_BOILING, hasHeatSource(pLevel, pPos) && pState.getValue(FILL_LEVEL) > 0);
+        return pState.setValue(IS_BOILING, hasHeatSource(pLevel, pPos));
     }
 
     public boolean hasHeatSource(LevelAccessor accessor, BlockPos pos)
@@ -101,5 +116,15 @@ public class CrucibleBlock extends Block implements EntityBlock
         Block block = state.getBlock();
 
         return block instanceof FireBlock || (block instanceof CampfireBlock && state.getValue(CampfireBlock.LIT)) || block instanceof SoulFireBlock || block instanceof TorchBlock || state.getFluidState().is(FluidTags.LAVA) || block instanceof NitorBlock;
+    }
+
+    @Nullable
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntity) {
+        return createTickerHelper(blockEntity, BlockEntityInit.CRUCIBLE.get(), CrucibleBlockEntity::tick);
+    }
+
+    @Override
+    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return BlockEntityInit.CRUCIBLE.get().create(pos, state);
     }
 }
