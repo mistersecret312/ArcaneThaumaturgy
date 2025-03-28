85721a0ef62eae744bafc5227db00759f219ba22
SGUDestiny <panormov2006@gmail.com>
2025-03-28 20:48:38 +0400

    Essentia Jars and stuff

--- src/main/java/com/mistersecret312/thaumaturgy/init/SoundInit.java
+++ src/main/java/com/mistersecret312/thaumaturgy/init/SoundInit.java
@@ -12,6 +12,8 @@ public class SoundInit
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, ArcaneThaumaturgyMod.MODID);
 
     public static RegistryObject<SoundEvent> WAND_USE = registerSoundEvent("wand_use");
+    public static RegistryObject<SoundEvent> ESSENTIA_JAR_OPEN = registerSoundEvent("essentia_jar_open");
+    public static RegistryObject<SoundEvent> ESSENTIA_JAR_CLOSE = registerSoundEvent("essentia_jar_close");
 
     private static RegistryObject<SoundEvent> registerSoundEvent(String sound)
     {
--- src/main/java/com/mistersecret312/thaumaturgy/init/SoundTypeInit.java
+++ src/main/java/com/mistersecret312/thaumaturgy/init/SoundTypeInit.java
@@ -7,4 +7,6 @@ import net.minecraftforge.common.util.ForgeSoundType;
 public class SoundTypeInit
 {
     public static final SoundType NITOR = new ForgeSoundType(1.0F, 1.0F, () -> SoundEvents.FIRE_EXTINGUISH, () -> SoundEvents.EMPTY, () -> SoundEvents.FIRECHARGE_USE, () -> SoundEvents.EMPTY, () -> SoundEvents.EMPTY);
+
+    public static final SoundType ESSENTIA_JAR = new ForgeSoundType(1.0F, 1.0F, () -> SoundInit.ESSENTIA_JAR_OPEN.get(), () -> SoundEvents.GLASS_STEP, () -> SoundInit.ESSENTIA_JAR_OPEN.get(), () -> SoundEvents.EMPTY, () -> SoundEvents.GLASS_FALL);
 }
