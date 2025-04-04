bff0d96bfbb1f4a624e7db21e1afdadfe531d89a
mistersecret312 <sergiigamer7@gmail.com>
2025-04-04 19:32:38 +0300

    Webhook test.

---
+++ src/main/java/com/mistersecret312/thaumaturgy/compatability/jei/ArcaneThaumaturgyJEIPlugin.java
@@ -0,0 +1,14 @@
+package com.mistersecret312.thaumaturgy.compatability.jei;
+
+import mezz.jei.api.IModPlugin;
+import net.minecraft.resources.ResourceLocation;
+
+public class ArcaneThaumaturgyJEIPlugin implements IModPlugin
+{
+
+    @Override
+    public ResourceLocation getPluginUid()
+    {
+        return null;
+    }
+}
