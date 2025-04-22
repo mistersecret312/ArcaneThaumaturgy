e9cdf8f6a2281a97ad36c4f7aaf4b392d21ca90e
SGUDestiny <panormov2006@gmail.com>
2025-04-22 23:02:06 +0400

    Veneum and Frigus aspect icons, greatwood compositions

--- src/main/java/com/mistersecret312/thaumaturgy/init/AspectInit.java
+++ src/main/java/com/mistersecret312/thaumaturgy/init/AspectInit.java
@@ -34,7 +34,7 @@ public class AspectInit
     public static final RegistryObject<Aspect> PERMUTATIO = ASPECTS.register("permutatio", () -> new Aspect(List.of(0xCC, 0x59, 0x33), ResourceLocation.parse("thaumaturgy:textures/aspect/permutatio.png")).withCompound(AspectInit.ORDO.get(), AspectInit.PERDITIO.get()));
     public static final RegistryObject<Aspect> VACUOS = ASPECTS.register("vacuos", () -> new Aspect(List.of(0x50, 0x55, 0x73), ResourceLocation.parse("thaumaturgy:textures/aspect/vacuos.png")).withCompound(AspectInit.PERDITIO.get(), AspectInit.AER.get()));
     public static final RegistryObject<Aspect> VITREUS = ASPECTS.register("vitreus", () -> new Aspect(List.of(0xB8, 0xE5, 0xDF), ResourceLocation.parse("thaumaturgy:textures/aspect/vitreus.png")).withCompound(AspectInit.TERRA.get(), AspectInit.ORDO.get()));
-    public static final RegistryObject<Aspect> VENEUM = ASPECTS.register("veneum", () -> new Aspect(List.of(0xB8, 0xE5, 0xDF), ResourceLocation.parse("thaumaturgy:textures/aspect/veneum.png")).withCompound(AspectInit.AQUA.get(), AspectInit.PERDITIO.get()));
+    public static final RegistryObject<Aspect> VENEUM = ASPECTS.register("veneum", () -> new Aspect(List.of(0x40, 0x80, 0x4A), ResourceLocation.parse("thaumaturgy:textures/aspect/veneum.png")).withCompound(AspectInit.AQUA.get(), AspectInit.PERDITIO.get()));
 
     //Other Aspects
     public static final RegistryObject<Aspect> HERBA = ASPECTS.register("herba", () -> new Aspect(List.of(0x1, 0xAC, 0x0), ResourceLocation.parse("thaumaturgy:textures/aspect/herba.png")).withCompound(AspectInit.VICTUS.get(), AspectInit.TERRA.get()));
@@ -59,7 +59,7 @@ public class AspectInit
     public static final RegistryObject<Aspect> MACHINA = ASPECTS.register("machina", () -> new Aspect(List.of(0xCC, 0x59, 0x33), ResourceLocation.parse("thaumaturgy:textures/aspect/machina.png")).withCompound(AspectInit.FACERE.get(), AspectInit.MOTUS.get()));
     public static final RegistryObject<Aspect> PROHIBITUM = ASPECTS.register("prohibitum", () -> new Aspect(List.of(0xCC, 0x33, 0x33), ResourceLocation.parse("thaumaturgy:textures/aspect/prohibitum.png")).withCompound(AspectInit.PRAECANTATIO.get(), AspectInit.TENEBRAE.get()));
     public static final RegistryObject<Aspect> ITER = ASPECTS.register("iter", () -> new Aspect(List.of(0xCC, 0x59, 0x33), ResourceLocation.parse("thaumaturgy:textures/aspect/iter.png")).withCompound(AspectInit.MOTUS.get(), AspectInit.TERRA.get()));
-    public static final RegistryObject<Aspect> FRIGUS = ASPECTS.register("frigus", () -> new Aspect(List.of(0xCC, 0x59, 0x33), ResourceLocation.parse("thaumaturgy:textures/aspect/frigus.png")).withCompound(AspectInit.AQUA.get(), AspectInit.VITREUS.get()));
+    public static final RegistryObject<Aspect> FRIGUS = ASPECTS.register("frigus", () -> new Aspect(List.of(0x17, 0xE5, 0xE5), ResourceLocation.parse("thaumaturgy:textures/aspect/frigus.png")).withCompound(AspectInit.AQUA.get(), AspectInit.VITREUS.get()));
     public static final RegistryObject<Aspect> VINCULUM = ASPECTS.register("vinculum", () -> new Aspect(List.of(0xCC, 0x59, 0x33), ResourceLocation.parse("thaumaturgy:textures/aspect/vinculum.png")).withCompound(AspectInit.MOTUS.get(), AspectInit.PERDITIO.get()));
     public static final RegistryObject<Aspect> VOLATUS = ASPECTS.register("volatus", () -> new Aspect(List.of(0xCC, 0x59, 0x33), ResourceLocation.parse("thaumaturgy:textures/aspect/volatus.png")).withCompound(AspectInit.MOTUS.get(), AspectInit.AER.get()));
 
